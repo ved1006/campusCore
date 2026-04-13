fb92d9af534ae6d1d2785dc50224d44b77f2fe10
Ved <dangeved106@gmail.com>
2026-04-13 13:13:40 +0530

    ADDING CYCLE

--- src/main/java/com/ved/students/service/BService.java
+++ src/main/java/com/ved/students/service/BService.java
@@ -1,17 +1,15 @@
-/*
- * package com.ved.students.service;
- * 
- * import org.springframework.beans.factory.annotation.Autowired;
- * import org.springframework.stereotype.Service;
- * 
- * @Service
- * public class BService {
- * 
- * @Autowired
- * private AService aService;
- * 
- * public void doSomething() {
- * aService.callB();
- * }
- * }
- */
+
+package com.ved.students.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BService {
+
+    private AService aService;
+
+    public void doSomething() {
+        aService.callB();
+    }
+}
