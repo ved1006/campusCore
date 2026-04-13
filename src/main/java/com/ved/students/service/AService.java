06a6a717ad7a448e071440adb1782a4127e62971
Ved <dangeved106@gmail.com>
2026-04-13 09:45:52 +0530

    configuring the original files

--- src/main/java/com/ved/students/service/AService.java
+++ src/main/java/com/ved/students/service/AService.java
@@ -1,15 +1,17 @@
-package com.ved.students.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AService {
-
-    @Autowired
-    private BService bService;
-
-    public void callB() {
-        bService.doSomething();
-    }
-}
+/*
+ * package com.ved.students.service;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.stereotype.Service;
+ * 
+ * @Service
+ * public class AService {
+ * 
+ * @Autowired
+ * private BService bService;
+ * 
+ * public void callB() {
+ * bService.doSomething();
+ * }
+ * }
+ */
